7457900868abd764083bdaf728fc439e766b3d60
lillian <keynesiane@gmail.com>
2025-05-12 12:01:08 -0700

    edit profile

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -25,10 +25,12 @@ const Profile = () => {
 
     const [profile, setProfile] = useState({});
     const [person, setPerson] = useState({});
+    const [bio, setBio] = useState("");
     const [follows, setFollows] = useState([]);
     const [posts, setPosts] = useState([]);
     const [error, setError] = useState(false);
     const [load, setLoading] = useState(true);
+    const [edit, setEdit] = useState(false);
 
      useEffect(() => {
             fetch(`http://localhost:3000/profile/${id}`,
@@ -36,47 +38,96 @@ const Profile = () => {
                     credentials: 'include',
                  })
               .then((response) => response.json())
-              .then((response) => {console.log(response.profile); setProfile(response.profile); setPerson(response.profile.user); setFollows(response.profile.user.followedBy); setPosts(response.profile.user.posts);})
+              .then((response) => {setProfile(response.profile); setBio(response.profile.bio); setPerson(response.profile.user); setFollows(response.profile.user.followedBy); setPosts(response.profile.user.posts); form.setFieldValue('bio', response.profile.bio);})
               .catch((error) => setError(error))
               .finally(() => setLoading(false));
           }, [id]);
 
-          const toggleFollow = async (id) => {
-            try {
-                const follow = await fetch(`http://localhost:3000/user/${id}/follow`,
-                    {
-                    mode: "cors" ,
-                    credentials: 'include',
-                    method: "POST",
-                    }
-                );
-                console.log(follow);
-    
+    const toggleFollow = async (id) => {
+    try {
+        const follow = await fetch(`http://localhost:3000/user/${id}/follow`,
+            {
+            mode: "cors" ,
+            credentials: 'include',
+            method: "POST",
             }
-            catch(err) {
-                console.error('Error toggling follow', err);
-            }
-        }
+        );
+        console.log(follow);
 
-          const postscards = 
-          !error && !load && posts ? posts.map((post) => (
-            <div key={post.id}>
-                <Post
-                id={post.id}
-                content={post.content}
-                createdAt={post.createdAt}
-                comment={post.comment.length}
-                like={post.like.length}
-                />
-            </div>
-          )) : null;
+    }
+    catch(err) {
+        console.error('Error toggling follow', err);
+    }
+}
+
+    const postscards = 
+    !error && !load && posts ? posts.map((post) => (
+    <div key={post.id}>
+        <Post
+        id={post.id}
+        content={post.content}
+        createdAt={post.createdAt}
+        comment={post.comment.length}
+        like={post.like.length}
+        />
+    </div>
+    )) : null;
+
+    const form = useForm({
+        mode: 'uncontrolled',
+        initialValues: {
+          bio: '',
+        },
+      });
+
+    const handleEdit = () => {
+        setEdit(true);
+    }
+
+    const cancelEdit = () => {
+        setEdit(false);
+    }
+
+    const submitEdit = async (id) => {
+        try {
+            const formData = form.getValues();
+            const profile = await fetch(`http://localhost:3000/profile/${id}/update`,
+                {
+                mode: "cors" ,
+                credentials: 'include',
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                  },
+                body: JSON.stringify(formData),
+                }
+            );
+            const data = await profile.json();
+            setBio(data.profile.bio);
+            setEdit(false);
+
+        }
+        catch(err) {
+            console.error('Error editing profile', err);
+        }
+    }
 
     return (<div>
         <img
   src={profile.avatar} />
   <p>{user.username}</p>
   <Button onClick={() => toggleFollow(user.id)}>Follow</Button>
-  <p>{profile.bio}</p>
+ {user.username === person.username && edit ? 
+ <form onSubmit={(e) => {e.preventDefault(); submitEdit(profile.id);}}>
+    <Textarea
+    {...form.getInputProps('bio')}
+    key={form.key('bio')}/>
+    <Button onClick={cancelEdit}>Cancel</Button>
+    <Button type="submit">Submit</Button>
+ </form> : 
+ user.username === person.username && !edit ?
+ <div><p>{bio}</p>
+ <Button onClick={handleEdit}>Edit</Button></div> : <p>{bio}</p>}
   <div>{postscards}</div>
     </div>)
 }
