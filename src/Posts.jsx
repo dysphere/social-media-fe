0c7405b13befc4464d7edcb3fc6db3f014375103
lillian <keynesiane@gmail.com>
2025-05-14 13:54:31 -0700

    toggle like posts

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -7,21 +7,32 @@ import { useForm } from "@mantine/form";
 
 //redirect to new post page after making post
 
-const Post = ({id, content, author, createdAt, comment, like}) => {
+const Post = ({id, content, author, createdAt, comment, like, handleLike, liked}) => {
     const postLink = `/post/${id}`
 
     return (<div>
         <p>{content}</p>
-        <p>{createdAt}</p>
-        <p>{author}</p>
-        <Link to={postLink}>{comment} comment(s)</Link>
-        <p>{like} like(s)</p>
+        <p>Posted: {createdAt}</p>
+        <p>By: {author}</p>
+        <div className="flex flex-row gap-4">
+        <div className="flex flex-row gap-1">
+        {liked ? <button onClick={handleLike} className="size-4">
+  <img src="src/assets/heart-black.svg" alt="Button Image"/>
+    </button> : 
+        <button onClick={handleLike} className="size-4">
+  <img src="src/assets/heart-outline.svg" alt="Button Image"/>
+    </button>}
+    <p>{like} like(s)</p>
+    </div>
+    <Link to={postLink}>{comment} comment(s)</Link>
+    </div>
     </div>)
 }
 
 const Posts = () => {
 
     const navigate = useNavigate();
+    const { user } = useContext(AuthContext);
 
     const [posts, setPosts] = useState([]);
     const [error, setError] = useState(false);
@@ -70,6 +81,50 @@ const Posts = () => {
         }
     }
 
+    const ToggleLike = async (id) => {
+        try {
+            await fetch(`http://localhost:3000/post/${id}/like`,
+                {
+                mode: "cors" ,
+                credentials: 'include',
+                method: "PUT",
+                }
+            );
+            const like_include_user = like_include(id);
+            if (like_include_user) {
+                  setPosts(posts.map((post) => {
+                    if (post.id === id) {
+                        return {
+                            ...post,
+                            like: post.like.filter((liker) => 
+                                liker.username !== user.username),
+                            };
+                        }
+                        return post;
+                        }));
+            }
+            else {
+                 setPosts(posts.map((post) => {
+                    if (post.id === id) {
+                    return {
+                        ...post,
+                        like: [...post.like, { user: user }], 
+                    };
+                    }
+                    return post;
+                }));
+            }
+        }
+        catch(err) {
+            console.error('Error toggling like', err);
+        }
+    }
+
+    const like_include = (id) => {
+        return posts.some((post) => {return post.id === id && 
+            post.like.some((liker) => {return user.username === liker.username})});
+    }
+
     const postscards = 
           !error && !load && posts ? posts.map((post) => (
             <div key={post.id}>
@@ -80,6 +135,8 @@ const Posts = () => {
                 author={post.author.username}
                 comment={post.comment.length}
                 like={post.like.length}
+                handleLike={() => ToggleLike(post.id)}
+                liked={like_include(post.id)}
                 />
             </div>
           )) : null;
